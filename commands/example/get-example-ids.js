21e756ae38b1b166623ac5a391318531d1f67725
Shp1nat <sergeev.stanislav03@gmail.com>
2025-06-02 01:32:23 +0300

    v1

--- commands/example/get-example-ids.js
+++ commands/example/get-example-ids.js
@@ -30,20 +30,87 @@ class GetExampleIds extends BaseGetIds {
         return new Set(publishers.flatMap(p => p.examples.map(e => e.id)));
     }
 
+    getPopularObjs (orders, cond) {
+        const frequencyMap = new Map();
+        for (const order of orders) {
+            if (order.examples && Array.isArray(order.examples)) {
+                for (const example of order.examples) {
+                    const itemsToCount = example[cond];
+                    if (Array.isArray(itemsToCount)) {
+                        for (const item of itemsToCount) {
+                            if (item?.id)
+                                frequencyMap.set(item.id, (frequencyMap.get(item.id) || 0) + 1);
+                        }
+                    }
+                }
+            }
+        }
+        const sortedIds = Array.from(frequencyMap.entries()).sort((a, b) => b[1] - a[1]).map(entry => entry[0]);
+        return sortedIds.slice(0, 3);
+    }
+
+    async getExampleIdsSetByRecsCond (cond, userId) {
+        const orders = await this.model.Order.findAll({
+            attributes: ['id'],
+            where: { userId: userId },
+            include: {
+                model: this.model.Example,
+                as: 'examples',
+                attributes: ['id'],
+                separate: true,
+                include: [
+                    {
+                        model: this.model.Author,
+                        as: 'authors',
+                        attributes: ['id'],
+                        through: {attributes: []}
+                    },
+                    {
+                        model: this.model.Genre,
+                        as: 'genres',
+                        attributes: ['id'],
+                        through: {attributes: []}
+                    },
+                    {
+                        model: this.model.Type,
+                        as: 'types',
+                        attributes: ['id'],
+                        through: {attributes: []}
+                    }
+                ],
+                through: {attributes: []}
+            }
+        });
+        const popularObjs = this.getPopularObjs(orders, cond); //top3 ids
+
+        //todo logic
+    }
+
     getPublisherCond (conditions) {
         const publisherCondIndex = conditions.findIndex(c => c.var === 'publisherName');
 
         return (publisherCondIndex !== -1) ? conditions.splice(publisherCondIndex, 1)[0].value : null;
     }
 
+    getRecsCond (conditions) {
+        const recsTypeCondIndex = conditions.findIndex(c => c.var === 'recsType');
+
+        return (recsTypeCondIndex !== -1) ? conditions.splice(recsTypeCondIndex, 1)[0].value : null;
+    }
+
     async executeGetterIds (inData) {
         const publisherCond = this.getPublisherCond(inData.conditions);
+        const recsCond = this.getRecsCond(inData.conditions);
         const query = this.prepareQuery(inData);
         const examples = await this.model.Example.findAll(query);
         let resultIds = examples.map(example => example.id);
         if (publisherCond) {
             const publisherExampleIdsSet  = await this.getExampleIdsSetByPublisherCond(publisherCond);
             resultIds = resultIds.filter(id => publisherExampleIdsSet.has(id));
+        }
+        if (recsCond && inData.userId) {
+            const recsExampleIdsSet  = await this.getExampleIdsSetByRecsCond(recsCond, inData.userId);
+            resultIds = resultIds.filter(id => recsExampleIdsSet.has(id));
         }
         return { rows: resultIds.slice(0, 1000), subtype: 'example' };
     }
